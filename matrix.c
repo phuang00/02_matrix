1c1e37f3e9848f3ee6e16e8cb57c1b3b2a904e4b
phuang00 <phuang00@stuy.edu>
2020-02-23 11:16:24 -0500

    wrote print matrix

--- matrix.c
+++ matrix.c
@@ -22,6 +22,16 @@ print the matrix such that it looks like
 the template in the top comment
 */
 void print_matrix(struct matrix *m) {
+  int i, j;
+  for (i = 0; i < m->rows; i++){
+    for (j = 0; j < m->lastcol; j++){
+      printf("%0.2lf ", m->m[i][j]);
+      if (j == m->lastcol - 1){
+        printf("\n");
+      }
+      printf("%d %d\n", i, j);
+    }
+  }
 }
 
 /*-------------- void ident() --------------
@@ -98,14 +108,14 @@ void free_matrix(struct matrix *m) {
 
 /*======== void grow_matrix() ==========
 Inputs:  struct matrix *m
-         int newcols 
-Returns: 
+         int newcols
+Returns:
 
 Reallocates the memory for m->m such that it now has
 newcols number of collumns
 ====================*/
 void grow_matrix(struct matrix *m, int newcols) {
-  
+
   int i;
   for (i=0;i<m->rows;i++) {
       m->m[i] = realloc(m->m[i],newcols*sizeof(double));
@@ -116,8 +126,8 @@ void grow_matrix(struct matrix *m, int newcols) {
 
 /*-------------- void copy_matrix() --------------
 Inputs:  struct matrix *a
-         struct matrix *b 
-Returns: 
+         struct matrix *b
+Returns:
 
 copy matrix a to matrix b
 */
@@ -125,7 +135,7 @@ void copy_matrix(struct matrix *a, struct matrix *b) {
 
   int r, c;
 
-  for (r=0; r < a->rows; r++) 
-    for (c=0; c < a->cols; c++)  
-      b->m[r][c] = a->m[r][c];  
+  for (r=0; r < a->rows; r++)
+    for (c=0; c < a->cols; c++)
+      b->m[r][c] = a->m[r][c];
 }
